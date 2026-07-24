31b53bf9c810f4bd0ed43aed5901211e36db622e
FarihaRehmann <fariharehman27>
2026-07-24 22:58:52 +0500

    Initial commit

---
+++ chapter3/app.js
@@ -0,0 +1,29 @@
+
+// Chapter 3 - Variables for Numbers
+
+// Q1
+let age = 16;
+alert("I am " + age + " years old.");
+
+// Q2
+let visitCount = 14;
+document.write("You have visited this site " + visitCount + " times.<br><br>");
+
+// Q3
+let birthYear = 2010;
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is number.<br><br>");
+
+// Q4
+let visitorName = "John Doe";
+let productTitle = "T-shirt(s)";
+let quantity = 5;
+
+document.write(
+  visitorName +
+    " ordered " +
+    quantity +
+    " " +
+    productTitle +
+    " on XYZ Clothing store."
+);
---
+++ chapter4/app.js
@@ -0,0 +1,36 @@
+// Chapter 4 - Variable Names: Legal & Illegal
+// Q1
+var firstName = "Fariha",
+    lastName = "Rehman",
+    age = 16;
+
+
+// Q2
+
+// var userName;
+// var _name;
+// var $price;
+// var myVariable;
+// var student1;
+
+
+// 5 Illegal Variable Names (Examples)
+
+// var 1name;
+// var my-name;
+// var my name;
+// var var;
+// var @email;
+
+
+// Q3
+
+document.write("<h1>Rules for naming JS variables</h1>");
+
+document.write("Variable names can only contain <b>letters</b>, <b>numbers</b>, <b>$</b> and <b>_</b>. For example: <b>$my_1stVariable</b><br><br>");
+
+document.write("Variables must begin with a <b>letter</b>, <b>$</b> or <b>_</b>. For example: <b>$name</b>, <b>_name</b> or <b>name</b><br><br>");
+
+document.write("Variable names are case <b>sensitive</b>.<br><br>");
+
+document.write("Variable names should not be JS <b>keywords</b>.");
